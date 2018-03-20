98dcb95f6f8b86d3a9fa031ba2a8a30cd5d75bb6
panda <panda@pandadeMacBook-Pro.local>
2018-03-20 23:24:37 +0800

    PrintKey

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,12 +2,28 @@
  * Created by panda on 2018/3/20.
  */
 import org.nem.core.crypto.KeyPair;
+import org.nem.core.model.Address;
+import org.nem.core.model.NetworkInfos;
+
 public class App {
 
         public static void main(String[] args) {
             System.out.println("nem:");
             final KeyPair someKey = new KeyPair();
-            System.out.println(String.format("Private key: %s", someKey.getPrivateKey()));
-            System.out.println(String.format("Public key: %s", someKey.getPublicKey()));
+            final Address anaddress=Address.fromPublicKey(NetworkInfos.getMainNetworkInfo().getVersion(), someKey.getPublicKey());
+
+            PrintKey(someKey,anaddress);
+
+            return;
+
         }
+
+
+    private static void PrintKey(KeyPair someKey, Address anaddress) {
+
+        System.out.println(String.format("Private key: %s", someKey.getPrivateKey()));
+        System.out.println(String.format("Public key: %s", someKey.getPublicKey()));
+        System.out.println(String.format("Address: %s",anaddress ));
+
+    }
 }
